0c4d0095026d501373f69c3eb01b0cceb2797a41
Jesse Hallett <jesse@sitr.us>
2024-03-23 15:51:07 -0700

    add nix devshell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems }:
+    let
+      eachSystem = callback: nixpkgs.lib.genAttrs (import systems) (system: callback (pkgs system));
+      pkgs = system: nixpkgs.legacyPackages.${system};
+    in
+    { 
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            nodePackages.pnpm
+            typescript
+          ];
+        };
+      });
+    };
+
+}
